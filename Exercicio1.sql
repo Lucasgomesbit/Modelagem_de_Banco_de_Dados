eae211577312d6cd740e1ed8481a32f544c859ae
Lucasgomesbit <lucaskelvin.paz26@gmail.com>
2026-08-18 18:35:19 -0300

    Update Exercicio1.sql

--- Exercicio1.sql
+++ Exercicio1.sql
@@ -17,16 +17,16 @@ CREATE TABLE Cursos (
 );
 SHOW TABLES;
 -- Inserindo registros na tabela Alunos
-INSERT INTO Alunos VALUES (1, 'Ana Silva', 20, 'São Paulo', 'ana.silva@email.com'); 
-INSERT INTO Alunos VALUES (2, 'Bruno Souza', 22, 'Rio de Janeiro', 'bruno.souza@email.com'); 
-INSERT INTO Alunos VALUES (3, 'Carla Mendes', 19, 'Belo Horizonte', 'carla.mendes@email.com'); 
-INSERT INTO Alunos VALUES (4, 'Daniel Lima', 23, 'Curitiba', 'daniel.lima@email.com'); 
-INSERT INTO Alunos VALUES (5, 'Eduarda Costa', 21, 'Salvador', 'eduarda.costa@email.com'); 
-INSERT INTO Alunos VALUES (6, 'Felipe Rocha', 24, 'Fortaleza', 'felipe.rocha@email.com'); 
-INSERT INTO Alunos VALUES (7, 'Gabriela Alves', 20, 'Recife', 'gabriela.alves@email.com'); 
-INSERT INTO Alunos VALUES (8, 'Henrique Martins', 25, 'Brasília', 'henrique.martins@email.com'); 
-INSERT INTO Alunos VALUES (9, 'Isabela Santos', 18, 'Manaus', 'isabela.santos@email.com'); 
-INSERT INTO Alunos VALUES (10, 'João Pereira', 22, 'Porto Alegre', 'joao.pereira@email.com');  
+INSERT INTO Alunos VALUES (1, 'Ana Silva', 20, 'São Paulo', 'ana.silva@gmail.com'); 
+INSERT INTO Alunos VALUES (2, 'Bruno Souza', 22, 'Rio de Janeiro', 'bruno.souza@gmail.com'); 
+INSERT INTO Alunos VALUES (3, 'Carla Mendes', 19, 'Belo Horizonte', 'carla.mendes@gmail.com'); 
+INSERT INTO Alunos VALUES (4, 'Daniel Lima', 23, 'Curitiba', 'daniel.lima@gmail.com'); 
+INSERT INTO Alunos VALUES (5, 'Eduarda Costa', 21, 'Salvador', 'eduarda.costa@gmail.com'); 
+INSERT INTO Alunos VALUES (6, 'Felipe Rocha', 24, 'Fortaleza', 'felipe.rocha@gmail.com'); 
+INSERT INTO Alunos VALUES (7, 'Gabriela Alves', 20, 'Recife', 'gabriela.alves@gmail.com'); 
+INSERT INTO Alunos VALUES (8, 'Henrique Martins', 25, 'Brasília', 'henrique.martins@gmail.com'); 
+INSERT INTO Alunos VALUES (9, 'Isabela Santos', 18, 'Manaus', 'isabela.santos@gmail.com'); 
+INSERT INTO Alunos VALUES (10, 'João Pereira', 22, 'Porto Alegre', 'joao.pereira@gmail.com');  
 
 -- Inserindo registros na tabela Cursos
 INSERT INTO Cursos VALUES (1, 'Banco de Dados', 60, 'Presencial', 800.00);
